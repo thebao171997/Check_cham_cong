f4fd9710387f6dae9ebd65a26f3e19447325c686
thebao171997 <thebao171997@gmail.com>
2026-06-22 13:52:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Check_cham_cong/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
